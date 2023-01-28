9c281e199a2e5cef137bb30c47bfa03a55a97f5d
liz <liyulg0@gmail.com>
2023-01-28 12:35:49 -0800

    "Added routes and server folders"

---
+++ server/routes/user.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { ObjectID } = require('bson');
+let Users= require('../models/hospital.model')
+
+router.route('/').get((reg, res) => {
+    Users.find()                             
+    .then(users => res.json(users))       
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+router.route('/add').post((req,res) => {
+    const userName = req.body.userName;
+    const password = req.body.password;
+    const new_user = new Users({
+        userName,
+        password,
+    });
+    new_user.save()
+    .then(()=> res.json('User added'))
+    .catch(err=> res.status(400).json('Error' + err));
+});
