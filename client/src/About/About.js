a39066832692e7185437477e90ba02a9aad959b0
Eric <eric.dianhao.wu@gmail.com>
2023-01-28 16:14:56 -0800

    client: add a margin to about page

--- client/src/About/About.js
+++ client/src/About/About.js
@@ -1,20 +1,21 @@
-import { List, ListItem } from "@mui/material";
+import { List, ListItem, Box } from "@mui/material";
 import React from "react";
 
 export default function Root() {
-  return <div>
-    <p>We are at the 22nd table! </p>
-    <List>Developers: 
-    <ListItem>Annie Lu</ListItem>
-    <ListItem>Kyle Machnicki</ListItem>
-    <ListItem>Eric Wu</ListItem>
-    <ListItem>Liyu Zerihun</ListItem>
-    </List>
-    <p>This project is a work-in-progress. 
-      Ideally, users would be able to sign up for an account, 
-      login, view hospital information, and add comments if 
-      they are registered. 
-    </p>
-    </div>;
-
+  return (
+    <Box m={2}>
+      <p>We are at the 22nd table! </p>
+      <List>Developers: 
+      <ListItem>Annie Lu</ListItem>
+      <ListItem>Kyle Machnicki</ListItem>
+      <ListItem>Eric Wu</ListItem>
+      <ListItem>Liyu Zerihun</ListItem>
+      </List>
+      <p>This project is a work-in-progress. 
+        Ideally, users would be able to sign up for an account, 
+        login, view hospital information, and add comments if 
+        they are registered. 
+      </p>
+    </Box>
+  );
 }
