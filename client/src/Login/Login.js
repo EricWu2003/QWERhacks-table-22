3d66b0760cb7d5f47437c8492b4521ba6d97da77
Kyle M <kmachnickiwork@gmail.com>
2023-01-29 00:20:18 -0800

    Preparing for backend interaction

--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -2,11 +2,12 @@ import * as React from 'react';
 import 
 {
   Stack, Button, TextField, Box, FormControl,
-  InputLabel, InputAdornment, OutlinedInput, Typography, Link
+  InputLabel, InputAdornment, OutlinedInput, Typography, Link, getBackdropUtilityClass
 } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import axios from "axios";
 
 export default function Login() {
   const [showPassword, setShowPassword] = React.useState(false);
@@ -21,24 +22,36 @@ export default function Login() {
 
   const handleClickLogin = () => {
     // Get info from username and password fields
-    // Access backend dict of username (key) --> password, ensure pass = pass
-    // Simple equality compare
-    if (0 == 1) 
-    {
-      // If username and password match backend
-      // Tell backend that user is logged in
-
-      // redirect to home page
-      window.location.href="/";
-    } else {
-      // If no match
-      // clear current user/pass inputs (useful or no?)
-      setUserName(""); // Need to attach to text field
-      setPassword(""); // Need to attach to text field
-
-      // error message appears
-      alert("Username and password do not match any known login credentials.");
+    // If username and password match backend
+    // Tell backend that user is logged in
+    const requestBody = {
+      username: userName,
+      password: password
     }
+    axios.get('http://localhost:8000/users/', requestBody)
+    .then(function (response) {
+      // Hardcode response.data type as Array
+      // Find from array by user+pass key
+      // If match, login, if no match, restart
+      console.log(response.data);
+      if (0==1) {
+        alert("Successfully logged in!")
+        window.location.href="/";
+      } else {
+        // If no match
+        // clear current user/pass inputs (useful or no?)
+        setUserName(""); // Need to attach to text field
+        setPassword(""); // Need to attach to text field
+        //window.location.href="/login";
+
+        // error message appears
+        alert("Username and password do not match any known login credentials.");
+      }
+    })
+    .catch(function (error) {
+      alert("Login failed")
+      console.log(error);
+    });
   }
 
   return (
@@ -47,7 +60,8 @@ export default function Login() {
         <img src="https://www.qwerhacks.com/media/frog.svg" height="50px" width="50px"/>
 
         {/* Input for username */}
-        <TextField label="Username" variant="outlined" onChange={event => setUserName(event.target.value)}/>
+        <TextField label="Username" variant="outlined" 
+        onChange={event => setUserName(event.target.value)}/>
 
         {/* Input for password */}
         <FormControl sx={{ m: 1 }} variant="outlined">
